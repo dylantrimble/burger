105053c28d90717f5318f3157e31a55611bb5238
dyl trimble <dyltrimble12@gmail.com>
2019-10-28 14:21:30 -0400

    added to the handlebars, trying to get public dir to sattach to main handlebars

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,29 @@
-var orm = require('../config/orm.js');
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
 var burger = {
-  selectAll: function(cb) {
-    orm.selectAll('burgers', function(res) {
-      cb(res);
-    });
-  },
-
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne('burgers', cols, vals, function(res) {
-      cb(res);
-    });
-  },
-
-  // The objColVals is an object specifying columns as object keys with associated values
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne('burgers', objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+    all: function(cb) {
+        orm.allOrder("burgers", "id", function(res) {
+            cb(res);
+        });
+    },
+    create: function(vals, cb) {
+        orm.create("burgers", ['burger_name'], vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
 };
 
+// Export the database functions for the controller (catsController.js).
 module.exports = burger;
+
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +16,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
