21926038ced2488f4fd4395e90f3e6be9548bf3d
dyl trimble <dyltrimble12@gmail.com>
2019-10-28 18:03:39 -0400

    added gif for main page

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 -- Creating seed data for the burgers_db database --
-INSERT INTO burgers (burger_name, devoured) ('Double Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) ('Hot Mess Burger', false);
-INSERT INTO burgers (burger_name, devoured) ('Road House Burger', false);
-INSERT INTO burgers (burger_name, devoured) ('Big Prime Brisket Burger', true);
-INSERT INTO burgers (burger_name, devoured) ('NC BBQ Burger', true);
-INSERT INTO burgers (burger_name, devoured) ('MecktoberFest Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Hot Mess Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Road House Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Big Prime Brisket Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('NC BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('MecktoberFest Burger', true);
