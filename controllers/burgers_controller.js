78c9973d79a837b1b9888525aff036dd3a11093f
dyl trimble <dyltrimble12@gmail.com>
2020-01-13 12:57:47 -0500

    cleaning up more js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,23 +2,21 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var hbsObject = {
             burgers: data
         };
-        // console.log(hbsObject);
+        
         res.render("index", hbsObject);
     });
 });
 
 router.post("/api/burgers", function(req, res) {
     burger.create([req.body.name], function(result) {
-        // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
 });
@@ -26,11 +24,8 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
-    // console.log("condition", condition);
-
     burger.update(req.body, condition, function(result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
           res.status(200).end();
@@ -43,7 +38,6 @@ router.delete("/api/burgers/:id", function(req, res) {
 
     burger.delete(condition, function(result) {
         if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -51,5 +45,4 @@ router.delete("/api/burgers/:id", function(req, res) {
     });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
